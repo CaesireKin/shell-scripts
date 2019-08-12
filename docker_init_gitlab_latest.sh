5ab32a1123d96c85f7858870edc9b7640785e221
jon <me@caesirekin.me>
2019-08-12 14:06:48 +0800

    Make docker_init_gitlab_latest.sh do the same specifications like other scripts

--- docker_init_gitlab_latest.sh
+++ docker_init_gitlab_latest.sh
@@ -12,4 +12,4 @@ echo 'Drop if there is already exist an instance ...'
 sudo docker container stop $container_name;
 sudo docker container rm $container_name;
 echo "Docker Volumes: $docker_volumes, starting initializing...";
-sudo docker run --name $container_name -p 443:443 -p 80:80 --restart always -v $volume_dir/config:/etc/gitlab -v $volume_dir/logs:/var/log/gitlab -v $volume_dir/data:/var/opt/gitlab -d gitlab/gitlab-ce;
+sudo docker run --name $container_name -p 443:443 -p 80:80 --restart always -v $volume_dir/$container_name/config:/etc/gitlab -v $volume_dir/$container_name/logs:/var/log/gitlab -v $volume_dir/$container_name/data:/var/opt/gitlab -d gitlab/gitlab-ce;
