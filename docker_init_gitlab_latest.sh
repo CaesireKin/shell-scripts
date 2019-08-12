9c23a8069903c497d7ad99ffe269c5f34c078de9
jon <me@caesirekin.me>
2019-08-12 14:31:35 +0800

    docker_init_gitlab_latest support --hostname configuration

--- docker_init_gitlab_latest.sh
+++ docker_init_gitlab_latest.sh
@@ -1,10 +1,11 @@
 # !/bin/bash
 
 container_name=$1;
-volume_dir=$2;
+hostname=$2
+volume_dir=$3;
 
 if [ ! $container_name ] || [ ! $volume_dir ]; then
-    echo 'Usage ./docker_init_gitlab.sh \${container_name} \${docker_volume_dir}';
+    echo 'Usage ./docker_init_gitlab.sh \${container_name} \${hostname} \${docker_volume_dir}';
     exit -1;
 fi
 
@@ -12,4 +13,4 @@ echo 'Drop if there is already exist an instance ...'
 sudo docker container stop $container_name;
 sudo docker container rm $container_name;
 echo "Docker Volumes: $docker_volumes, starting initializing...";
-sudo docker run --name $container_name -p 443:443 -p 80:80 --restart always -v $volume_dir/$container_name/config:/etc/gitlab -v $volume_dir/$container_name/logs:/var/log/gitlab -v $volume_dir/$container_name/data:/var/opt/gitlab -d gitlab/gitlab-ce;
+sudo docker run --name $container_name --hostname $hostname -p 443:443 -p 80:80 --restart always -v $volume_dir/$container_name/config:/etc/gitlab -v $volume_dir/$container_name/logs:/var/log/gitlab -v $volume_dir/$container_name/data:/var/opt/gitlab -d gitlab/gitlab-ce;
