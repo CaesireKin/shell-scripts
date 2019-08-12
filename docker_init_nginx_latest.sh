bb35c28f7d86ac454cbe119672783320d8f953b1
jon <me@caesirekin.me>
2019-08-12 14:01:59 +0800

    Add Nginx Initialize Scription‘

---
+++ docker_init_nginx_latest.sh
@@ -0,0 +1,13 @@
+# !/bin/bash
+
+container_name=$1;
+volume_dir=$2;
+
+if [ ! $container_name ] || [ ! $volume_dir ]; then
+    echo 'Usage ./docker_init_gitlab.sh \${container_name} \${docker_volume_dir}';
+    exit -1;
+fi
+
+docker container stop $container_name;
+docker container rm $container_name;
+docker run --name $container_name -p 80:80 -p 443:443 -v $docker_volumes/$container_name/html:/usr/share/nginx/html:ro -v $docker_volumes/$container_name/conf/:/etc/nginx.conf:ro -d nginx;
