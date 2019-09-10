dafc355b99682e80059140e9e85310775b41f297
jon <me@caesirekin.me>
2019-09-10 17:23:50 +0800

    Teamcity已加入

---
+++ docker_init_teamcity_latest.sh
@@ -0,0 +1,14 @@
+# !/bin/bash
+
+container_name=$1
+docker_volumes=$2;
+
+if [ ! $container_name ] || [ ! $docker_volumes ]; then
+  echo "Usage: ./docker_init_teamcity_latest.sh \${container_name} \${docker_volumes}";
+  exit -1;
+fi
+
+echo "Docker Volumes: $docker_volumes, starting initializing...";
+docker container stop $container_name;
+docker container rm $container_name;
+docker run --name $container_name -p 9111:8111 -v $docker_volumes/$container_name/2019.1.3/data:/data/teamcity_server/datadir -v $docker_volumes/$container_name/2019.1.3/logs:/opt/teamcity/logs -d jetbrains/teamcity-server
