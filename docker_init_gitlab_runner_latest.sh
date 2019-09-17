bb31d209f217aed10edbc166c01c7e97d0568a1b
jon <me@caesirekin.me>
2019-09-18 04:54:07 +0800

    即将实现gitlab-runner的脚本

--- docker_init_gitlab_runner_latest.sh
+++ docker_init_gitlab_runner_latest.sh
@@ -1,1 +1,15 @@
 
+# !/bin/bash
+
+container_name=$1;
+volume_dir=$2;
+
+if [ ! $container_name ] || [ ! $volume_dir ]; then
+    echo 'Usage ./docker_init_gitlab_runner_latest.sh \${container_name} \${volume_dir}';
+    exit -1;
+fi
+
+echo 'Drop if there is already exist an instance ...'
+sudo docker container stop $container_name;
+sudo docker container rm $container_name;
+echo "Docker Volumes: $volume_dir, starting initializing...";
