7dbbe4baa76322cc8a398002d27e27e6bbd4667e
caesarking@live.com <me@caesirekin.me>
2019-10-27 19:43:19 +0800

    Add MySQL Init script on windows (Powershell Only)

---
+++ docker_init_mysql_5.ps1
@@ -0,0 +1,13 @@
+container_name=$1
+docker_volumes=$2;
+mysql_root_password=$3;
+
+if (!($container_name) -or !(docker_volumes) -or !(mysql_root_password)) {
+  echo "Usage: ./docker_init_mysql_5.sh \${container_name} \${docker_volumes} \${mysql_root_password}";
+  exit -1;
+}
+
+echo "Docker Volumes: $docker_volumes, starting initializing...";
+docker container stop $container_name;
+docker container rm $container_name;
+docker run --name $container_name -p 3306:3306 -v $docker_volumes/$container_name/datas:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=$mysql_root_password --restart always -d mysql:5 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci;
