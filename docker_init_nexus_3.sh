0684540c71ec4f0c37c7bcca8f0462fa2f67a1f3
卓丶灼 <me@caesirekin.me>
2020-03-13 10:04:16 +0800

    新增nexus3初始化脚本

---
+++ docker_init_nexus_3.sh
@@ -0,0 +1,9 @@
+container_name=$1
+volume_path=$2
+admin_port=$3
+docker_registry=$4
+maven_registry=$5
+docker run --name $container_name \
+    -p $admin_port:8080 -p $docker_registry -p $maven_registry \
+    -v $volume_path/$container_name/nexus-data:/nexus-data \
+    --restart always -d sonatype/nexus3
